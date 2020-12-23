db5ea5d3f9846cd691e25493ebeae499b633c74b
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-12-23 20:30:24 +0100

    feat: day7 solutions

---
+++ src/main/java/nl/jiankai/Rule.java
@@ -0,0 +1,11 @@
+package nl.jiankai;
+
+public class Rule {
+    public int quantity;
+    public String bag;
+
+    public Rule(int quantity, String bag) {
+        this.quantity = quantity;
+        this.bag = bag;
+    }
+}
